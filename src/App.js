d26be522742268c079b47c835eb49914f2de4f5a
Nazmi <nazmifeeroz@hotmail.com>
2017-11-20 23:47:28 +0800

    adding notes works with firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Note from './Note/Note';
+import Noteform from './Noteform/Noteform';
+import {DB_CONFIG} from './config/config';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import './App.css';
 
 class App extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.addNote = this.addNote.bind(this);
+		this.app = firebase.initializeApp(DB_CONFIG);
+		this.database = this.app.database().ref().child('notes');
+		this.state = {
+			notes: [],
+		}
+	}
+	
+	componentWillMount(){
+		const previousNotes = this.state.notes;
+		this.database.on('child_added', snap => {
+			previousNotes.push({
+				id: snap.key, 
+				noteContent: snap.val().noteContent,
+			})
+			this.setState({
+				notes: previousNotes
+			})
+		})
+	}
+	
+	addNote(note){
+		this.database.push().set({noteContent: note});
+	}
+//	
+//	addNote(note) {
+//		const previousNotes = this.state.notes;
+//		previousNotes.push({ id: previousNotes.length + 1, noteContent: note });
+//		this.setState({
+//			notes: previousNotes
+//		})
+//	}
+	
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+			<div className="notesWrapper">
+				<div className="notesHeader">
+					<div className="heading">React &amp; Firebase To-Do List</div>
+				</div>
+				<div className="notesBody">
+				{
+					this.state.notes.map((note) => {
+						return (
+							<Note noteContent={note.noteContent} 
+							noteId={note.id} 
+							key={note.id}/>
+						)
+					})		
+				}
+				</div>
+				<div className="notesFooter">
+					<Noteform addNote={this.addNote} />
+				</div>
+			</div>
     );
   }
 }
---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyBDZQClRJlBSB8DyLIqfVUMnMro6xdqQdg",
+    authDomain: "react-notes-404ae.firebaseapp.com",
+    databaseURL: "https://react-notes-404ae.firebaseio.com",
+    projectId: "react-notes-404ae",
+    storageBucket: "react-notes-404ae.appspot.com",
+    messagingSenderId: "1089283028461"
+  };
